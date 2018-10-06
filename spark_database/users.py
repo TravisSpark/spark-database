e3ade18ca7dbc69d9ef60dd6f892c14ed26fd5df
tangoyankee <tangoyankee>
2018-10-06 01:09:43 -0700

    rename factories to uses

--- spark_database/users.py
+++ spark_database/users.py
@@ -1,16 +1,53 @@
-# from models import User
+"""
+Manage user information
+"""
+import models
+from pick import pick
 
-# class Users(User):
-#     def __init__(self, session):
-#         self.session = session
+def user_input():
+    """
+    Create new accounts from dashboard inputs
+    """
+    correct = 'no'
+    while correct == 'no':
+        first_name = input ("First Name: ")
+        last_name = input ("Last Name: ")
+        email = input ("Email: ")
+        phone = input ("Phone: ")
+        unit = input ("Unit: ")
+        section = input ("Section: ")
+        rfid = input ("RFID: ")
 
-#     def add_user(self, data):
-#         self.first_name = data["first_name"]
-#         self.last_name = data["last_name"]
-#         self.email = data["email"]
-#         User.phone = data["phone"]
-#         User.unit = data["unit"]
-#         User.section = data[""]
-#         self.section
+        check =  (f"first_name: {first_name}, last_name: {last_name}, email: {email}, phone: {phone}, unit: {unit}, section: {section}, rfid: {rfid}")
+        title = f"{check}\n Are these values correct?"
+        options = ['yes', 'no']
+        correct, index = pick(options, title)
 
-# User.rfid = "userf"
+    user_data = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+        'phone': phone,
+        'unit': unit,
+        'section': section,
+        'rfid': rfid
+    }
+    return user_data
+
+def add_user(session):
+    user_data = user_input()
+    session.add(models.User(**user_data))
+    print("added user data")
+
+def show_all_users(session):
+    all_users = session.query(models.User).all()
+    for user in all_users:
+        print(f"first_name: {user.first_name}, last_name: {user.last_name}, email: {user.email}, phone: {user.phone}, unit: {user.unit}, section: {user.section}, rfid: {user.rfid}, date joined: {user.date_joined}")
+
+def search_for_user(session):
+    search_first_name = input("What is the user's first name? ")
+    search_last_name = input("What is the user's last name? ")
+    user_search = session.query(models.User).filter(models.User.first_name.like(f"%{search_first_name}%"), models.User.last_name.like(f"%{search_last_name}%"))
+    
+    for user in user_search:
+        print(f"first_name: {user.first_name}, last_name: {user.last_name}, email: {user.email}, phone: {user.phone}, unit: {user.unit}, section: {user.section}, rfid: {user.rfid}")
